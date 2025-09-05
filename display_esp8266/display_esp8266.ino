0b733f549387f1df44bc085d203ae1caab919e3b
rootanvir <tanvir.22464751@gmail.com>
2025-09-05 22:30:10 +0600

    pir sensor

--- display_esp8266/display_esp8266.ino
+++ display_esp8266/display_esp8266.ino
@@ -9,11 +9,14 @@ typedef struct struct_message {
   float ax;
   float ay;
   float az;
+  char direction[10];
 } struct_message;
 
 struct_message accelData;
 
-// Callback: called when ESP-NOW data is received
+#define PIR_PIN D2
+bool pirState = false;
+
 void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&accelData, incomingData, sizeof(accelData));
 }
@@ -22,31 +25,39 @@ void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
 
-  if (esp_now_init() != 0) {
-    Serial.println("ESP-NOW init failed");
-    while (1);
-  }
-
+  if (esp_now_init() != 0) while(1);
   esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
   esp_now_register_recv_cb(OnDataRecv);
 
+  pinMode(PIR_PIN, INPUT);
   u8g2.begin();
 }
 
 void loop() {
+  pirState = digitalRead(PIR_PIN);
+
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_ncenB10_tr);
 
   char buffer[25];
-
   snprintf(buffer, sizeof(buffer), "X: %.2f", accelData.ax);
-  u8g2.drawStr(0, 20, buffer);
+  u8g2.drawStr(0, 15, buffer);
 
   snprintf(buffer, sizeof(buffer), "Y: %.2f", accelData.ay);
-  u8g2.drawStr(0, 35, buffer);
+  u8g2.drawStr(0, 30, buffer);
 
   snprintf(buffer, sizeof(buffer), "Z: %.2f", accelData.az);
-  u8g2.drawStr(0, 50, buffer);
+  u8g2.drawStr(0, 45, buffer);
+
+  snprintf(buffer, sizeof(buffer), "Dir: %s", accelData.direction);
+  u8g2.drawStr(0, 60, buffer);
+
+  // PIR motion status
+  if (pirState) {
+    u8g2.drawStr(70, 15, "yes");
+  } else {
+    u8g2.drawStr(70, 15, "no");
+  }
 
   u8g2.sendBuffer();
   delay(100);
--- gyro_esp32/gyro_esp32.ino
+++ gyro_esp32/gyro_esp32.ino
@@ -10,57 +10,83 @@ typedef struct struct_message {
   float ax;
   float ay;
   float az;
+  char direction[15];
 } struct_message;
 
 struct_message accelData;
 
-// Replace with ESP8266 MAC address of your receiver
+// EMA smoothing
+float alpha = 0.1;
+float axSmooth = 0, aySmooth = 0, azSmooth = 0;
+
+// Vibration motor pin
+#define MOTOR_PIN 5  // GPIO5
+
+// ESP8266 MAC address
 uint8_t receiverMAC[] = {0x34, 0x5F, 0x45, 0x55, 0xE4, 0x34};
 
 void setup() {
   Serial.begin(115200);
 
-  // Initialize MPU6050
   if (!mpu.begin()) {
     Serial.println("MPU6050 not found!");
-    while (1);
+    while(1);
   }
+
+  pinMode(MOTOR_PIN, OUTPUT);
+  digitalWrite(MOTOR_PIN, LOW);
 
   // Initialize ESP-NOW
   WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
     Serial.println("ESP-NOW init failed");
-    while (1);
+    while(1);
   }
 
-  // Add peer (ESP8266 receiver)
+  // Add ESP8266 receiver as peer
   esp_now_peer_info_t peerInfo = {};
   memcpy(peerInfo.peer_addr, receiverMAC, 6);
-  peerInfo.channel = 0;  
+  peerInfo.channel = 0;
   peerInfo.encrypt = false;
-
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add peer");
-    while (1);
-  }
+  esp_now_add_peer(&peerInfo);
 }
 
 void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  accelData.ax = a.acceleration.x;
-  accelData.ay = a.acceleration.y;
-  accelData.az = a.acceleration.z;
+  // EMA smoothing
+  axSmooth = alpha * a.acceleration.x + (1 - alpha) * axSmooth;
+  aySmooth = alpha * a.acceleration.y + (1 - alpha) * aySmooth;
+  azSmooth = alpha * a.acceleration.z + (1 - alpha) * azSmooth;
 
-  // Send data to ESP8266
-  esp_err_t result = esp_now_send(receiverMAC, (uint8_t *)&accelData, sizeof(accelData));
+  accelData.ax = axSmooth;
+  accelData.ay = aySmooth;
+  accelData.az = azSmooth;
 
-  if (result == ESP_OK) {
-    Serial.println("Sent successfully");
+  // Predict direction
+  if (azSmooth > 7) strcpy(accelData.direction, "Up");
+  else if (azSmooth < -7) strcpy(accelData.direction, "Down");
+  else if (axSmooth > 7) strcpy(accelData.direction, "Right");
+  else if (axSmooth < -7) strcpy(accelData.direction, "Left");
+  else if (aySmooth > 7) strcpy(accelData.direction, "Forward");
+  else if (aySmooth < -7) strcpy(accelData.direction, "Backward");
+  else strcpy(accelData.direction, "Loading...");
+
+  // Vibrate motor if flat
+  if (strcmp(accelData.direction, "Loading...") == 0) {
+    digitalWrite(MOTOR_PIN, HIGH);
   } else {
-    Serial.println("Error sending data");
+    digitalWrite(MOTOR_PIN, LOW);
   }
 
-  delay(100); // Send every 100ms
+  // Send data to ESP8266
+  esp_now_send(receiverMAC, (uint8_t *)&accelData, sizeof(accelData));
+
+  Serial.print("X="); Serial.print(axSmooth);
+  Serial.print(" Y="); Serial.print(aySmooth);
+  Serial.print(" Z="); Serial.print(azSmooth);
+  Serial.print(" Dir="); Serial.println(accelData.direction);
+
+  delay(100);
 }
